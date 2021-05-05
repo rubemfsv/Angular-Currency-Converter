38dc40eab325ec325d918b1da82db996dddf82b6
Rubem <rfsv@ic.ufal.br>
2021-05-05 20:31:49 -0300

    chore: implements pipes

--- src/app/converter/pipes/date-br.pipe.ts
+++ src/app/converter/pipes/date-br.pipe.ts
@@ -1,12 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dateBr'
+  name: 'dateBr',
 })
 export class DateBrPipe implements PipeTransform {
+  /**
+   * Apply date format
+   *
+   * @param string dateEn
+   * @returns string date in Br format (dd/mm/yyyy)
+   */
+  transform(dateEn: string): string {
+    if (!dateEn) {
+      return '';
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const dateArr = dateEn.split('-');
+
+    if (dateArr.length !== 3) {
+      return dateEn;
+    }
+
+    return dateArr[2] + '/' + dateArr[1] + '/' + dateArr[0];
   }
-
 }
