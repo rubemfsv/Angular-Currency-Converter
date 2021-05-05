19e0e16e5cd19b6d33c36c282070aab65f7297fb
Rubem <rfsv@ic.ufal.br>
2021-05-05 15:37:26 -0300

    chore: implements converter component ts

--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConverterComponent } from './';
 import { ConverterService, CurrencyService } from './services';
 
 @NgModule({
+  imports: [CommonModule, HttpClientModule, FormsModule],
   declarations: [ConverterComponent],
-  imports: [CommonModule, HttpClientModule],
   exports: [ConverterComponent],
   providers: [CurrencyService, ConverterService],
 })
